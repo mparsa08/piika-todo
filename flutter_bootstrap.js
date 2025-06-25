79d394c29567b3abc478c9b40e2659d969b8d1e4
Parsa Kermanian <kermanianparsa@gmail.com>
2025-06-25 16:09:21 +0330

    fix some bugs

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1320225023"
+    serviceWorkerVersion: "1304207245"
   }
 });
